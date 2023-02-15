84338c708c9496e3d146ea8e7aa047093f5e089a
StefanKubsch <stefan.kubsch@outlook.de>
2023-02-15 08:48:38 +0100

    Small update

--- ClassChunky/Compile_ClassChunkyDLL.ps1
+++ ClassChunky/Compile_ClassChunkyDLL.ps1
@@ -1,4 +1,4 @@
 ﻿$path = Resolve-Path ClassChunky.cs  
-$compiler = "$env:windir/Microsoft.NET/Framework/v2.0.50727/csc"  
+$compiler = "$env:windir/Microsoft.NET/Framework/v4.0.30319/csc"  
 &$compiler /target:library ClassChunky.cs /unsafe 
 dir ClassChunky*
